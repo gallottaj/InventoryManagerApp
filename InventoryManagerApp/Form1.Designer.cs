0946659f7bc0b93deba41f90fde0a24cd79bef0c
Joey <gallottaj@hotmail.com>
2020-05-02 15:07:34 -0700

    change add item

--- InventoryManagerApp/Form1.Designer.cs
+++ InventoryManagerApp/Form1.Designer.cs
@@ -28,13 +28,190 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.itemIDTextBox = new System.Windows.Forms.TextBox();
+            this.itemNameTextBox = new System.Windows.Forms.TextBox();
+            this.itemDescriptionTextBox = new System.Windows.Forms.TextBox();
+            this.manufacturerIDTextBox = new System.Windows.Forms.TextBox();
+            this.quantityTextBox = new System.Windows.Forms.TextBox();
+            this.storageLocationTextBox = new System.Windows.Forms.TextBox();
+            this.addItemButton = new System.Windows.Forms.Button();
+            this.inventoryListBox = new System.Windows.Forms.ListBox();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.removeButton = new System.Windows.Forms.Button();
+            this.selectButton = new System.Windows.Forms.Button();
+            this.editButton = new System.Windows.Forms.Button();
+            this.searchButton = new System.Windows.Forms.Button();
+            this.addedItemListBox = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // itemIDTextBox
+            // 
+            this.itemIDTextBox.Location = new System.Drawing.Point(106, 129);
+            this.itemIDTextBox.Name = "itemIDTextBox";
+            this.itemIDTextBox.Size = new System.Drawing.Size(100, 20);
+            this.itemIDTextBox.TabIndex = 0;
+            this.itemIDTextBox.Text = "Item ID";
+            this.itemIDTextBox.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // itemNameTextBox
+            // 
+            this.itemNameTextBox.Location = new System.Drawing.Point(106, 169);
+            this.itemNameTextBox.Name = "itemNameTextBox";
+            this.itemNameTextBox.Size = new System.Drawing.Size(100, 20);
+            this.itemNameTextBox.TabIndex = 1;
+            this.itemNameTextBox.Text = "Name";
+            // 
+            // itemDescriptionTextBox
+            // 
+            this.itemDescriptionTextBox.Location = new System.Drawing.Point(106, 209);
+            this.itemDescriptionTextBox.Name = "itemDescriptionTextBox";
+            this.itemDescriptionTextBox.Size = new System.Drawing.Size(100, 20);
+            this.itemDescriptionTextBox.TabIndex = 2;
+            this.itemDescriptionTextBox.Text = "Description";
+            // 
+            // manufacturerIDTextBox
+            // 
+            this.manufacturerIDTextBox.Location = new System.Drawing.Point(106, 248);
+            this.manufacturerIDTextBox.Name = "manufacturerIDTextBox";
+            this.manufacturerIDTextBox.Size = new System.Drawing.Size(100, 20);
+            this.manufacturerIDTextBox.TabIndex = 3;
+            this.manufacturerIDTextBox.Text = "Manufacturer ID";
+            // 
+            // quantityTextBox
+            // 
+            this.quantityTextBox.Location = new System.Drawing.Point(106, 291);
+            this.quantityTextBox.Name = "quantityTextBox";
+            this.quantityTextBox.Size = new System.Drawing.Size(100, 20);
+            this.quantityTextBox.TabIndex = 5;
+            this.quantityTextBox.Text = "Quantity";
+            // 
+            // storageLocationTextBox
+            // 
+            this.storageLocationTextBox.Location = new System.Drawing.Point(106, 326);
+            this.storageLocationTextBox.Name = "storageLocationTextBox";
+            this.storageLocationTextBox.Size = new System.Drawing.Size(100, 20);
+            this.storageLocationTextBox.TabIndex = 6;
+            this.storageLocationTextBox.Text = "Storage Location";
+            // 
+            // addItemButton
+            // 
+            this.addItemButton.Location = new System.Drawing.Point(261, 291);
+            this.addItemButton.Name = "addItemButton";
+            this.addItemButton.Size = new System.Drawing.Size(75, 23);
+            this.addItemButton.TabIndex = 7;
+            this.addItemButton.Text = "ADD ITEM";
+            this.addItemButton.UseVisualStyleBackColor = true;
+            this.addItemButton.Click += new System.EventHandler(this.addItemButton_Click);
+            // 
+            // inventoryListBox
+            // 
+            this.inventoryListBox.FormattingEnabled = true;
+            this.inventoryListBox.Location = new System.Drawing.Point(551, 129);
+            this.inventoryListBox.Name = "inventoryListBox";
+            this.inventoryListBox.Size = new System.Drawing.Size(137, 147);
+            this.inventoryListBox.TabIndex = 8;
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(426, 321);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(75, 23);
+            this.exitButton.TabIndex = 10;
+            this.exitButton.Text = "EXIT";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // removeButton
+            // 
+            this.removeButton.Location = new System.Drawing.Point(342, 320);
+            this.removeButton.Name = "removeButton";
+            this.removeButton.Size = new System.Drawing.Size(75, 23);
+            this.removeButton.TabIndex = 11;
+            this.removeButton.Text = "REMOVE";
+            this.removeButton.UseVisualStyleBackColor = true;
+            this.removeButton.Click += new System.EventHandler(this.removeButton_Click);
+            // 
+            // selectButton
+            // 
+            this.selectButton.Location = new System.Drawing.Point(261, 320);
+            this.selectButton.Name = "selectButton";
+            this.selectButton.Size = new System.Drawing.Size(75, 23);
+            this.selectButton.TabIndex = 13;
+            this.selectButton.Text = "SELECT";
+            this.selectButton.UseVisualStyleBackColor = true;
+            this.selectButton.Click += new System.EventHandler(this.selectButton_Click);
+            // 
+            // editButton
+            // 
+            this.editButton.Location = new System.Drawing.Point(342, 291);
+            this.editButton.Name = "editButton";
+            this.editButton.Size = new System.Drawing.Size(75, 23);
+            this.editButton.TabIndex = 14;
+            this.editButton.Text = "RE-STOCK";
+            this.editButton.UseVisualStyleBackColor = true;
+            this.editButton.Click += new System.EventHandler(this.editButton_Click);
+            // 
+            // searchButton
+            // 
+            this.searchButton.Location = new System.Drawing.Point(426, 292);
+            this.searchButton.Name = "searchButton";
+            this.searchButton.Size = new System.Drawing.Size(75, 23);
+            this.searchButton.TabIndex = 15;
+            this.searchButton.Text = "SEARCH";
+            this.searchButton.UseVisualStyleBackColor = true;
+            this.searchButton.Click += new System.EventHandler(this.searchButton_Click);
+            // 
+            // addedItemListBox
+            // 
+            this.addedItemListBox.FormattingEnabled = true;
+            this.addedItemListBox.Location = new System.Drawing.Point(261, 129);
+            this.addedItemListBox.Name = "addedItemListBox";
+            this.addedItemListBox.Size = new System.Drawing.Size(240, 147);
+            this.addedItemListBox.TabIndex = 16;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.addedItemListBox);
+            this.Controls.Add(this.searchButton);
+            this.Controls.Add(this.editButton);
+            this.Controls.Add(this.selectButton);
+            this.Controls.Add(this.removeButton);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.inventoryListBox);
+            this.Controls.Add(this.addItemButton);
+            this.Controls.Add(this.storageLocationTextBox);
+            this.Controls.Add(this.quantityTextBox);
+            this.Controls.Add(this.manufacturerIDTextBox);
+            this.Controls.Add(this.itemDescriptionTextBox);
+            this.Controls.Add(this.itemNameTextBox);
+            this.Controls.Add(this.itemIDTextBox);
+            this.Name = "Form1";
+            this.Text = "Inventory Manager";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox itemIDTextBox;
+        private System.Windows.Forms.TextBox itemNameTextBox;
+        private System.Windows.Forms.TextBox itemDescriptionTextBox;
+        private System.Windows.Forms.TextBox manufacturerIDTextBox;
+        private System.Windows.Forms.TextBox quantityTextBox;
+        private System.Windows.Forms.TextBox storageLocationTextBox;
+        private System.Windows.Forms.Button addItemButton;
+        private System.Windows.Forms.ListBox inventoryListBox;
+        private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.Button removeButton;
+        private System.Windows.Forms.Button selectButton;
+        private System.Windows.Forms.Button editButton;
+        private System.Windows.Forms.Button searchButton;
+        private System.Windows.Forms.ListBox addedItemListBox;
     }
 }
 
--- InventoryManagerApp/Form1.cs
+++ InventoryManagerApp/Form1.cs
@@ -12,9 +12,219 @@ namespace InventoryManagerApp
 {
     public partial class Form1 : Form
     {
+        List<Inventory> inventoryList = new List<Inventory>();
         public Form1()
         {
             InitializeComponent();
         }
+
+        private void InventoryItem(Inventory item)
+        {
+            //get item ID
+            if (int.TryParse(itemIDTextBox.Text, out int itemID))
+            {
+                item.ItemID = itemID;
+            }
+            else
+            {
+                MessageBox.Show("Invalid");
+            }
+
+            //get item name
+            item.ItemName = itemNameTextBox.Text;
+
+            //get item description
+            item.ItemDescription = itemDescriptionTextBox.Text;
+
+            //get manufacturer ID
+            item.ManufacturerID = manufacturerIDTextBox.Text;
+
+            //get quantity in stock
+            if (int.TryParse(quantityTextBox.Text, out int quantity))
+            {
+                item.Quantity = quantity;
+            }
+            else
+            {
+                MessageBox.Show("Invalid");
+            }
+
+            //get item storage location
+            item.StorageLocation = storageLocationTextBox.Text;
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void addItemButton_Click(object sender, EventArgs e)
+        {
+
+
+                addedItemListBox.Text = itemIDTextBox.Text;
+                addedItemListBox.Text = itemNameTextBox.Text;
+                addedItemListBox.Text = itemDescriptionTextBox.Text;
+                addedItemListBox.Text = manufacturerIDTextBox.Text;
+                addedItemListBox.Text = quantityTextBox.Text;
+                addedItemListBox.Text = storageLocationTextBox.Text;
+
+                ClearTextBoxes();
+
+        }
+
+        private void ClearTextBoxes()
+        {
+            itemIDTextBox.Clear();
+            itemNameTextBox.Clear();
+            itemDescriptionTextBox.Clear();
+            manufacturerIDTextBox.Clear();
+            quantityTextBox.Clear();
+            storageLocationTextBox.Clear();
+            itemNameTextBox.Focus();
+
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void removeButton_Click(object sender, EventArgs e)
+        {
+            int index = inventoryListBox.SelectedIndex;
+
+            if (inventoryListBox.SelectedIndex == -1)
+                MessageBox.Show("Must select an item first!");
+
+            inventoryList[index].ItemID = 0;
+            inventoryList[index].ItemName = "";
+            inventoryList[index].ItemDescription = "";
+            inventoryList[index].ManufacturerID = "";
+            inventoryList[index].Quantity = 0;
+            inventoryList[index].StorageLocation = "";
+
+            inventoryListBox.Items.Remove(inventoryList[index].ItemID);
+
+
+            ClearTextBoxes();
+        }
+
+        private void selectButton_Click(object sender, EventArgs e)
+        {
+            int index = inventoryListBox.SelectedIndex;
+            if (inventoryListBox.SelectedIndex == -1)
+                MessageBox.Show("Must select an item first!");
+
+            itemIDTextBox.Text = (inventoryList[index].ItemID.ToString());
+            itemNameTextBox.Text = inventoryList[index].ItemName;
+            itemDescriptionTextBox.Text = inventoryList[index].ItemDescription;
+            manufacturerIDTextBox.Text = inventoryList[index].ManufacturerID;
+            quantityTextBox.Text = (inventoryList[index].Quantity.ToString());
+            storageLocationTextBox.Text = inventoryList[index].StorageLocation;
+
+        }
+
+        private void editButton_Click(object sender, EventArgs e)
+        {
+            // index of item
+            int index = inventoryListBox.SelectedIndex;
+
+            if (inventoryListBox.SelectedIndex == -1)
+                MessageBox.Show("Must select an item first!");
+
+            // show field
+            if (itemIDTextBox.Text != (inventoryList[index].ItemID.ToString()))
+            {
+                if (int.TryParse(itemIDTextBox.Text, out int ID))
+                { inventoryList[index].ItemID = ID; }
+                else
+                {   // error
+                    MessageBox.Show("Invalid");
+                }
+            }
+
+            if (itemNameTextBox.Text != inventoryList[index].ItemName)
+            { inventoryList[index].ItemDescription = itemNameTextBox.Text; }
+
+            if (itemDescriptionTextBox.Text != inventoryList[index].ItemDescription)
+            { inventoryList[index].ItemDescription = itemDescriptionTextBox.Text; }
+
+            if (manufacturerIDTextBox.Text != inventoryList[index].ManufacturerID)
+            { inventoryList[index].ManufacturerID = manufacturerIDTextBox.Text; }
+
+            if (itemIDTextBox.Text != (inventoryList[index].ItemID.ToString()))
+            {
+                if (int.TryParse(itemIDTextBox.Text, out int ID))
+                { inventoryList[index].ItemID = ID; }
+                else
+                {   // error
+                    MessageBox.Show("Invalid");
+                }
+            }
+
+            if (quantityTextBox.Text != inventoryList[index].Quantity.ToString())
+            {
+                if (int.TryParse(quantityTextBox.Text, out int quantity))
+                { inventoryList[index].Quantity = quantity; }
+                else
+                {   // error
+                    MessageBox.Show("Invalid");
+                }
+            }
+            storageLocationTextBox.Text = inventoryList[index].StorageLocation;
+
+
+            ClearTextBoxes();
+        }
+
+        private void searchButton_Click(object sender, EventArgs e)
+        {
+            //search by item name or item ID
+
+            //error
+            if (String.IsNullOrEmpty(itemNameTextBox.Text))
+            {
+                MessageBox.Show("Please enter an item name.");
+            }
+
+            if (!String.IsNullOrEmpty(itemNameTextBox.Text))
+            {
+                TextBox1_TextChanged();
+            }
+
+            //error
+            if (String.IsNullOrEmpty(itemIDTextBox.Text))
+            {
+                MessageBox.Show("Please enter an item name.");
+            }
+
+            if (!String.IsNullOrEmpty(itemIDTextBox.Text))
+            {
+                TextBox1_TextChanged();
+
+            }
+        }
+
+        private void TextBox1_TextChanged()
+
+        {
+            int index = inventoryListBox.FindString(this.itemNameTextBox.Text);
+            if (0 <= index)
+            {
+                inventoryListBox.SelectedIndex = index;
+
+                itemNameTextBox.Text = inventoryList[index].ItemName;
+
+            }
+        }
     }
 }
+  
+
